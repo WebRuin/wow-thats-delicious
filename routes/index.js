a2ada3e62983fe2e302442fcbeaeb76f0b1b4d70
webruin <tim.smith.hdg@gmail.com>
2018-04-15 02:20:44 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  // res.send('Hey! It works!');
+  res.render('hello', {
+    title: 'hi',
+  })
+});
+
+module.exports = router;
